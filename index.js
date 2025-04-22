dc8ce67b1edb0278f0eb7243a8e9cbc6e22af88a
not-sol <jtcastones@up.edu.ph>
2025-04-22 23:40:14 +0800

    HTMLs

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const form = document.querySelector('.form');
 const input = document.getElementById('city-input');
 const apiKey = '9c35b0b6acecc3036c9032d3349a5136';
-const statusDisplay = document.querySelector('.status');
 
 form.addEventListener('submit', async e => {
-    statusDisplay.innerHTML = ''
     e.preventDefault();
+
+    input.classList.remove('is-invalid');
+    form.classList.add('was-validated');
+
+    if (!form.checkValidity()) {
+        return;
+    }
 
     const cityName = input.value;
 
-    if (cityName) {
-        try {
-            const data = await getData(cityName);
-            displayWeather(data);
-        } catch (error) {
-            console.error(error);
-        }
-    } else {
-        statusDisplay.innerHTML = 'Please input a City';
+    try {
+        const data = await getData(cityName);
+        displayWeather(data);
+    } catch (error) {
+        console.error(error);
     }
 });
 
@@ -27,6 +28,8 @@ async function getData(cityName) {
     const apiResponse = await fetch(apiUrl);
 
     if (!apiResponse.ok) {
+        form.classList.remove('was-validated');
+        input.classList.add('is-invalid');
         throw new Error('Could not fetch weather data');
     }
 
@@ -43,8 +46,8 @@ function displayWeather(data) {
     const {name: cityName, main: {temp, humidity}, weather: [{description, icon}]} = data;
 
     nameDisplay.innerHTML = cityName;
-    tempDisplay.innerHTML = temp;
-    humidityDisplay.innerHTML = humidity;
+    tempDisplay.innerHTML = `Temperature: ${(temp - 273.15).toFixed(2)}°C`;
+    humidityDisplay.innerHTML = `Humidity: ${humidity}%`;
     descriptionDisplay.innerHTML = description;
     iconDisplay.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 }

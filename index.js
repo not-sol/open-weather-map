d488c0b25255a2425f53885afebe22754921d2cd
not-sol <jtcastones@up.edu.ph>
2025-04-23 10:08:29 +0800

    comments ig

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const input = document.getElementById('city-input');
 const display = document.querySelector('.display');
 const apiKey = '9c35b0b6acecc3036c9032d3349a5136';
 
-form.addEventListener('submit', async e => {
-    e.preventDefault();
+form.addEventListener('submit', async e => { // search button function
+    e.preventDefault(); // preventing from refreshing the page
 
     input.classList.remove('is-invalid');
     form.classList.add('was-validated');
 
     if (!form.checkValidity()) {
-        return;
+        return; // return if input is empty don't execute
     }
 
     const cityName = input.value;
@@ -24,12 +24,12 @@ form.addEventListener('submit', async e => {
     }
 });
 
-async function getData(cityName) {
+async function getData(cityName) { // sends a http request to the API
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
 
     const apiResponse = await fetch(apiUrl);
 
-    if (!apiResponse.ok) {
+    if (!apiResponse.ok) { // check if api response ok is false
         form.classList.remove('was-validated');
         input.classList.add('is-invalid');
         throw new Error('Could not fetch weather data');
@@ -38,14 +38,14 @@ async function getData(cityName) {
     return await apiResponse.json();
 }
 
-function displayWeather(data) {
+function displayWeather(data) { // displays the weather data 
     const nameDisplay = document.querySelector('.name');
     const tempDisplay = document.querySelector('.temp');
     const humidityDisplay = document.querySelector('.humidity');
     const descriptionDisplay = document.querySelector('.description');
     const iconDisplay = document.querySelector('.icon');
 
-    const {name: cityName, main: {temp, humidity}, weather: [{description, icon}]} = data;
+    const {name: cityName, main: {temp, humidity}, weather: [{description, icon}]} = data; // 
 
     nameDisplay.innerHTML = cityName;
     tempDisplay.innerHTML = `Temperature: ${(temp - 273.15).toFixed(2)}°C`;
